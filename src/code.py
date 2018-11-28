038d7021936e17f9af24f6e9c1718c0475f20cc8
aljalou2 <aljalou2@illinois.edu>
2018-11-28 15:49:18 +0300

    data cleaning done

--- src/code.py
+++ src/code.py
@@ -1,5 +1,30 @@
 import pandas as pd 
 
-data = pd.read_csv('data/BlackFriday.csv')
+df = pd.read_csv('data/BlackFriday.csv')
 
-data['Gender'] = data['Gender'].map({'F':0,'M':1})
+# @param col: column to do the mapping for
+# @return: the column after being mapped to a range
+def map_column(col):
+	unique_vals = df[col].unique()
+	df[col] = df[col].replace({unique_vals[i] : i for i in range(unique_vals.shape[0])})
+
+
+purchase_total = df.groupby(df.User_ID)['Purchase'].sum()
+df = df.drop_duplicates('User_ID')
+df = df.set_index('User_ID')
+df['Purchase_Total'] = pd.Series(a)
+
+df.drop(['Product_ID',
+	'Product_Category_1',
+	'Product_Category_2',
+	'Product_Category_3',
+	'Purchase'], axis=1)
+
+#mapping catigorical data to a range
+df['Gender'] = map_column('Gender')
+df['Age'] = map_column('Age')
+df['Occupation'] = map_column('Occupation')
+df['City_Category'] = map_column('City_Category')
+df['Stay_In_Current_City_Years'] = map_column('Stay_In_Current_City_Years')
+
+
